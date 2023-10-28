2824a157f9e72845fee168522b5819ba5630c6dc
fabiodrneles <fabiodrneles@gmail.com>
2023-10-28 01:17:41 -0300

    chore: refactor footer component and header component

--- devmall/src/app/app.module.ts
+++ devmall/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FooterComponent } from './footer/footer.component'; 
+import { MatBadgeModule } from '@angular/material/badge'; 
 
 
 
@@ -28,7 +29,8 @@ import { FooterComponent } from './footer/footer.component';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
