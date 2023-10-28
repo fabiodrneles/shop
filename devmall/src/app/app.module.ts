668646762712e4f50736c40633bb09ddd28922a1
fabiodrneles <fabiodrneles@gmail.com>
2023-10-27 21:15:23 -0300

    feat: add footer component

--- devmall/src/app/app.module.ts
+++ devmall/src/app/app.module.ts
@@ -9,14 +9,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FooterComponent } from './footer/footer.component'; 
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
